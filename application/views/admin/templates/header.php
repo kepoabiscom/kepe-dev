6c9bdfb804e41d7b20696d36c228c539e654db1c
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-06-08 07:09:26 -0400

    Testing git-ftp on linux

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -10,7 +10,7 @@
 <!-- Top Menu Items -->
 <ul class="nav navbar-right top-nav">
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"> </i>&nbsp;<?php $data = $this->session->userdata('logged_in'); echo $data['username']; ?><b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"> </i>&nbsp; <?php $data = $this->session->userdata('logged_in'); echo $data['username']; ?><b class="caret"></b></a>
         <ul class="dropdown-menu">
             <li>
                 <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
