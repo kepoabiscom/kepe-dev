6cc388c9bdd2d1992e6abebeb05992919293d90d
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-06-15 17:09:20 -0400

    Bug fix youtube_id and fix wording

--- application/views/admin/profile/update_password.php
+++ application/views/admin/profile/update_password.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     
-    <title>User Management - KepoAbis.com</title>
+    <title>Profile - KepoAbis.com</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
