7d92aeeed6ba893f92af7790ee064dad4c083890
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-06-20 04:12:25 -0400

    Update API region weather on dashboard

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -27,7 +27,7 @@ class Dashboard extends CI_Controller {
 	}
  	
 	function get_api_weather() {
-		$city = "Jakarta";
+		$city = $this->get_region();
 		$jsonurl = "http://api.openweathermap.org/data/2.5/weather?q=" . $city;
 		$json = file_get_contents($jsonurl);
 
@@ -35,15 +35,30 @@ class Dashboard extends CI_Controller {
 		$kelvin = $parsed_json->main->temp;
 		$celcius = $kelvin - 273.15;
 		$weather = $parsed_json->weather[0]->main;
+		
+		return array("city" => $city,
+					"weather" => $weather,
+					"celcius" => floor($celcius),
+					"now" => $this->get_now()
+			);
+	}
+
+	function get_now() {
 		$month = array("January", "February", "March", "April", "May", "June",
 					"July", "August", "September", "October", "November", "December"
 			);
 		$now = date("l") . ", " . date("d") . " " . $month[(int) date("m")-1] . " " . date("Y");
-		return array("city" => $city,
-					"weather" => $weather,
-					"celcius" => floor($celcius),
-					"now" => $now
-			);
+		return $now;
+	}
+
+	function get_region() {
+		try {
+			$ip = $_SERVER['REMOTE_ADDR']; echo $ip;
+			$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+			return !isset($details->city) ? "Jakarta" : $details->city;
+		} catch(Exception $e) {
+			print_r($e);
+		}
 	}
 
 	function logout() {
