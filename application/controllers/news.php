4c7cfceb8ec4530d40949031877aecc557c4a7ac
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-07-11 23:20:22 +0700

    Update paging search result

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -96,7 +96,7 @@ class News extends CI_Controller {
 					"title" => "<a href='" . $read_more . "'>".$title."</a>",
 					"read_more" => "<a class='btn btn-primary' href='" . $read_more . "'>Read More</a>",
 					"summary" => !isset($q->summary) ? "" : $q->summary,
-					"body" => !isset($q->body) ? "" : $q->body,
+					"body" => !isset($q->body) ? "" : $this->get_short_summary($q->body),
 					"full_name" => !isset($q->nama_lengkap) ? "" : $q->nama_lengkap,
 					"created_date" => !isset($q->created_date) ? "" : $q->created_date,
 					"image" => $img,
@@ -245,6 +245,20 @@ class News extends CI_Controller {
 					."aria-label='Next'><span aria-hidden='true'>Next &raquo;</span></a></li>";
 			return $next;
 		}
+	}
+
+	function get_short_summary($text) {
+		$words = explode(" ", $text);
+		$N = 30;
+		if(count($words) > $N) {
+			$text = "";
+			for($i=0; $i < $N; $i++) {
+				$text .= $words[$i] . " ";
+			}
+			$text .= "[...]";
+		}
+
+		return $text;
 	}
 
 	function slug($str='') {
