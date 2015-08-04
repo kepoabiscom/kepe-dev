11a74a2297fb67e942f2a960f6789a6057fe969f
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-08-04 22:36:47 +0700

    Update sitemap xml format

---
+++ application/views/feeds/sitemap.php
@@ -0,0 +1,16 @@
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+<url>
+    <loc><?php echo base_url();?></loc>
+    <lastmod><?php echo date("Y-m-d"); ?></lastmod>
+    <priority>1.0</priority>
+    <changefreq>monthly</changefreq>
+</url>
+{get_sitemap}
+<url>
+	<loc>{loc}</loc>
+	<lastmod>{lastmod}</lastmod>
+	<priority>{priority}</priority>
+	<changefreq>{changefreq}</changefreq>
+ </url>
+ {/get_sitemap}
+</urlset>
