d0463abf658aa478159c533903e1dc4538fddf52
syahulramadhan <sramadhan95@gmail.com>
2015-08-08 11:35:33 +0700

    update query user order by user_id

--- application/views/index.php
+++ application/views/index.php
@@ -132,7 +132,7 @@
 		
 		google.maps.event.addDomListener(window, 'load', initialize);
 		
-		/*
+		
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -140,7 +140,7 @@
 
 		ga('create', 'UA-48862041-1', 'auto');
 		ga('send', 'pageview');
-		*/
+		
 	</script>
 	
     <!-- Bootstrap core JavaScript
