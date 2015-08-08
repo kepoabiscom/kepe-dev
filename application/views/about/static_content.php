d21d2d9ccf1b5c2bae62e3cb87cb2158886d768b
syahulramadhan <sramadhan95@gmail.com>
2015-08-08 12:24:18 +0700

    update grid out team

--- application/views/about/static_content.php
+++ application/views/about/static_content.php
@@ -17,15 +17,13 @@
 	</div>
 	<div class="blog-post">
 		<div class="row">
-		    <div class="col-lg-12">
 		    	{membership_list}
-		        <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+		        <div class="col-lg-3 col-md-4 col-xs-12 thumb">
 		            <a class="{thumbnail}" href="#" data-image-id="" data-toggle="modal" data-title="{nama_lengkap}" data-caption="{nama_lengkap}" data-image="{img}" data-target="#image-gallery">
 		                <img class="img-responsive" src="{img}" alt="{nama_lengkap}">
 		            </a>
 		        </div>
 		        {/membership_list}
-			</div>
 		<div class="modal fade" id="image-gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 		    <div class="modal-dialog">
 		        <div class="modal-content">
