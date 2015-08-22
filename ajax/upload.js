bf2fe6965e10c6f4502db905d64f6734af35103f
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-08-22 18:10:16 +0700

    update script ajax upload at gallery

--- ajax/upload.js
+++ ajax/upload.js
@@ -9,17 +9,18 @@ function uploadFile(file){
     var fd = new FormData();
     xhr.open("POST", url, true);
     xhr.onreadystatechange = function() {
-        if(xhr.readyState == 3 && xhr.status == 200) {
+        if(xhr.readyState == 4 && xhr.status == 200) {
             var fx = JSON.parse(xhr.responseText);
             document.getElementById("msg").innerHTML = "Loading...";
             setTimeout(function() { 
                 if(fx.status) {
                     document.getElementById("msg").innerHTML = "<span style='color:green'>" + fx.msg + "</span>";  
                     $('.img_bts').prepend(fx.get_img);
+                    $('.alert-success').html('');
                 } else {
                     document.getElementById("msg").innerHTML = "<span style='color:red'>" + fx.msg + "</span>";      
                 }
-            }, 2000);
+            }, 1000);
         }
     };
     fd.append("upload_img", file);
