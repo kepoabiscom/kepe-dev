a2b192a8a1a337eda0d63b8667c21f391cea4405
adilazka <azka.dhiyaan@gmail.com>
2015-09-26 21:28:02 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
- <form>
-First name:<br>
-<input type="text" name="firstname">
-<br>
-Last name:<br>
-<input type="text" name="lastname">
-</form> 
+<!DOCTYPE html>
+
+<html>
+	
+	<h1>kepoabis.com</h1>
+	<p>welcome to kepoabis.com.</p>
+
+</html>
