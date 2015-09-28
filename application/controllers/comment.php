4acfdbbd9de3a12e90e289aed20fd2b2ab03ea02
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-09-28 21:39:24 +0700

    Update #2

--- application/controllers/comment.php
+++ application/controllers/comment.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once APPPATH . "../facebook-sdk/config.php";
+require_once APPPATH . "../facebook-sdk/facebook.php";
 
 date_default_timezone_set("Asia/Jakarta");
 
@@ -151,8 +151,13 @@ class Comment extends CI_Controller {
 	}
 
 	function get_user_data_facebook() {
-		$config = new Config();
-		$is_active = $config->getUser();
+		$this->facebook = new Facebook(array(
+	        'appId'  => '876274572459160',
+	        'secret' => 'c44768470ff9f9d7a52784f6f5fbfd9a',
+      	));
+      	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = false;
+      	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 2;
+      	$is_active = $this->facebook->getUser();
 		$user_profile = ""; $loginUrl = "";
 
 		if($is_active) {
