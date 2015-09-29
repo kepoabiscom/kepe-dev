a6d8700969234726e7d6c571fc048892399bb1aa
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-09-29 21:31:33 +0700

    Update #05

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -167,6 +167,32 @@ class Article extends CI_Controller {
 		
 		if($q == null) show_404();
 		else {
+			$facebook = new Facebook(array(
+	        'appId'  => '876274572459160',
+	        'secret' => 'c44768470ff9f9d7a52784f6f5fbfd9a',
+	        'cookie' => true,
+	        'domain' => base_url()
+	      	));
+	      	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = false;
+	      	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 2;
+	      	$is_active = $facebook->getUser();
+			$user_profile = ""; $lUrl = "";
+
+			if($is_active) {
+				try {
+					$user_profile = $facebook->api('/me');
+				} catch (FacebookApiException $e) {
+					error_log($e);
+					$is_active = null;
+				}
+			}
+
+			if($is_active) {
+				$lUrl = $facebook->getLogoutUrl();
+			} else {
+				$lUrl = $facebook->getLoginUrl();
+			}
+
 			$comment = new Comment();
 
 			$prev_next = $this->prev_next($id);
@@ -208,7 +234,7 @@ class Article extends CI_Controller {
 			 				"n1" => $comment->random_set_captcha(0),
 			 				"op" => $comment->random_set_captcha(),
 			 				"n2" => $comment->random_set_captcha(0),
-			 				"login_url_fb" => $data_fb['login_fb_url'],
+			 				"login_url_fb" => $lUrl,
 			 				"prev_next" => $prev_next,
 			 				"article_id" => $id,
 							"count_article_comment" => $this->article_model->count_article_comment($id)->count_article_comment,
