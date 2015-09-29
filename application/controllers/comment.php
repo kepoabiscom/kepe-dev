c5fa84d47fc3bf7160bfab8644f1b85e6b36059e
Herman Wahyudi <herman.wahyudi02@gmail.com>
2015-09-29 22:18:14 +0700

    Update #11

--- application/controllers/comment.php
+++ application/controllers/comment.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once APPPATH . "../facebook-sdk/facebook.php";
+//require_once APPPATH . "../facebook-sdk/facebook.php";
 
 date_default_timezone_set("Asia/Jakarta");
 
@@ -15,7 +15,49 @@ class Comment extends CI_Controller {
 	}
 	
 	public function index() {
-		include("./././facebook-sdk/config.php");
+		require './././facebook-sdk/facebook.php';
+
+		$facebook = new Facebook(array(
+		  'appId'  => '876274572459160',
+		  'secret' => 'c44768470ff9f9d7a52784f6f5fbfd9a',
+		));
+		Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = false;
+		Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 2;
+
+		$user = $facebook->getUser();
+
+		if($user) {
+		  try {
+		    $user_profile = $facebook->api('/me');
+		  } catch (FacebookApiException $e) {
+		    error_log($e);
+		    $user = null;
+		  }
+		}
+
+		$url = "";
+
+		if($user) {
+		  $url = $facebook->getLogoutUrl();
+		} else {
+		  $url = $facebook->getLoginUrl();
+		}
+
+		if($user){
+		  echo "<a href=" . $url . ">Logout</a>";
+		} else{
+		  echo "<a href=" . $url .">Login with Facebook</a>";
+		}
+
+
+		print_r($_SESSION);
+
+		if ($user){
+		  echo "<img src='https://graph.facebook.com/'". $user. "/picture'>";
+		  print_r($user_profile);
+		} else {
+		  echo "<strong><em>You are not Connected.</em></strong>";
+		}
 	}
 
 	function ajax_() {
